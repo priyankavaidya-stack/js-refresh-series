7ddcf8bd7553403a6ee5f9b2f7e08a3417ceeb69
priyankavaidya-stack <60043222+priyankavaidya-stack@users.noreply.github.com>
2023-10-27 06:18:35 +0000

    variables and datatypes basics

--- .devcontainer/01_basicJs/test.js
+++ .devcontainer/01_basicJs/test.js
@@ -1,1 +1,29 @@
-console.log("priyanka");
+const accountId = 14544
+let accountEmail = "priyanka@google.com"
+var accountPassword = "12345"
+let accountCity;
+
+// accountId = 23
+
+accountEmail = "pv@gmail.com"
+accountPassword = "21345"
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// Data types and standards
+let name = "Priyanka"
+let age = 6
+let isLoggedIn = true
+
+// number 
+// string
+// bigint for big values like in stock trading
+// boolean
+// null => standalone value , we can assign empty value as null.
+// typeof null is object
+// undefined => typeof undefines is -> undefined
+// symbol => to find uniqueness in components
+// object
+
+// console.log(typeof null);  object
+
